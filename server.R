d3b24b20a8baae402f74417d4a7aeb3b34bc56e5
Martin Slíva <martinsliva@users.noreply.github.com>
2018-08-05 20:15:17 +0200

    New app

--- server.R
+++ server.R
@@ -1,40 +1,66 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
+
 
 library(shiny)
 library(ggplot2)
 library(plotly)
+library(tidyverse)
+library(rvest)
+library(RColorBrewer)
+library(ggthemes)
+library(ggalt)
+library(ggmap)
 
 ### loading data and setting variables and constants
+index <- read.csv("gpi.csv")  ### loading data
 
-data("mtcars")
+index$Country<-as.character(index$Country) ### defactoring
 
-a<- names(mtcars) 
+#### creating classes for map
 
-i <- 1
+breaks_map = c(0, 10, 20, 40, 60, 80, 100, 120, 140, 160, 170)
+labels_map = c("0-10", "10-20", "20-40", "40-60", "60-80", "80-100", "100-120", "120-140", "140-160", "160 +")
+
+index$Classes <- cut(index$Ranking, 
+                     breaks = breaks_map,
+                     labels = labels_map)
+
+world <- ggplot2::map_data("world") %>%
+  filter(region != "Antarctica")
 
 # Define server logic required to draw a chart and calculate regression parametrs
 shinyServer(function(input, output) {
-   
+  
   output$distPlot <- renderPlot({
     
     # input from ui.R
-    j <- as.numeric(input$select)
+    j <- input$religion
+    i <- input$religiosity
+    
+    if (i=="All"& j=="All") index_chart <- index 
+       else if (i=="All") index_chart <- index[index$Religion==j,]
+              else if (j=="All") index_chart <- index[index$Religiosity==i,]
+                      else index_chart <- index[index$Religion==j&index$Religiosity==i,]
+    
+  
+    
+    ### creating map
+    
+    ggplot() +
+      geom_map(data = world, map = world,
+               aes(x = long, y = lat, map_id = region),
+               col = "lightblue", fill= "lightgrey", size = 0.1) +
+      geom_map(data = index_chart, map = world,
+               aes(map_id = Country, fill = Classes)) +
+      ggalt::coord_proj("+proj=wintri") + # for Winkel Trimple projection
+      scale_fill_brewer(NULL, palette = "RdYlGn", direction = -1) +
+      ggthemes::theme_map() +
+      theme(plot.title = element_text(face = "bold")) +
+      labs(title = "Global Peace Index",
+           subtitle = "Countries sorted by rank",
+           caption = "Sources: Wikipedia, The Telegraph, Country Economy")
     
     
-    model <- lm(mtcars[,i]~mtcars[,j])
-    Intercept <- model[[1]][1]
-    Slope <- model[[1]][2]
     
-    plot(y=mtcars[,i], x=mtcars[,j], type="p", xlab = a[j], ylab = a[i], main = paste(a[i]," = (", round(Intercept,3), ") + (", round(Slope,3), ")*", a[j]), col="blue")+abline(Intercept,Slope, col="red")
-
-    
-  })
+  }, width = "auto", height = 720)
   
 })
--- ui.R
+++ ui.R
@@ -1,39 +1,52 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
+
 
 library(shiny)
 library(ggplot2)
 library(plotly)
+library(leaflet)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Scatter chart for mtcars"),
-  
+  titlePanel("Global Peace Index"),
+  helpText("mesures the relative position of nations peacefulness. "),
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-       selectInput("select",label = h3("Select predictor"),
-                   choices = list("cyl" = 2, "disp" = 3, "hp" = 4, "drat" = 5,"wt" = 6, "qsec" = 7, "vs" = 8, "am" = 9,"gear"= 10,"carb"=11 ), 
-                   selected = 2)
+      selectInput("religion",label = h3("Select main religion"),
+                  choices = list("All"= "All",   "Christian" = "Christian", "Islam" = "Islam", "Irreligion" = "Irreligion", "Hindu" = "Hindu","Buddhist" = "Buddhist", "Folk" = "Folk", "Jewish" = "Jewish" ), 
+                  selected = "All")
+    ,
+    
+    helpText("Main religion is religion with most followers in country. "),
+    
+    
+    selectInput("religiosity",label = h3("Select religiosity"),
+                choices = list("All"= "All",   "Most" = "most", "Middle" = "middle", "Least" = "least"), 
+                selected = "All"),
+   
+    helpText("Religiosty is defined as an answer to question if respondent feels religious. Most means that more than 95% respondent said yes. Least means that less than 40% respondend said no. ")
+    
     ),
     
-    # Show a plot of the generated distribution
+    
+   
+   # Show a plot of the generated distribution
     mainPanel(
-          tabsetPanel(type = "tabs", 
-                      tabPanel("Chart", br(), plotOutput("distPlot")), 
-                      tabPanel("Help", br(), "Chose predictor form select box. The chart shows linear regression for mpg. In the titile you can see the formula for regression line"), 
-                      tabPanel("Description", br(), "The application uses standard mtcars dataset from R. Server side calculates the linear regression for outcome mpg and chosen predictor.")
-                      
-          
-          )
+      tabsetPanel(type = "tabs", 
+                  tabPanel("Chart", br(), plotOutput("distPlot")), 
+                  tabPanel("Help", br(), "Chose religion or religiosity from select box.", br(),br(), "The map will be recalculated accordind to selection.", br(), "It may last some time, please be pattient."), 
+                  tabPanel("Description", br(), "The application shows how peacefull is each country measured by Global Peace Index. It allows to explore map according to two parametrs - Main religion and religiosity for each country.", br(), "It describes data from respected public resources.",br(), br(), 
+                           "Global Peace Index - data taken from ", tags$a(href="https://countryeconomy.com/demography/global-peace-index/", "Country Economy"), br(), br(),
+                           "Religiosity - data taken from  ", tags$a(href="https://www.telegraph.co.uk/travel/maps-and-graphics/most-religious-countries-in-the-world/", "The Telegraph"), br(), br(),
+                           "Main religion  - data taken from ", tags$a(href="https://en.wikipedia.org/wiki/Religions_by_country", "Wikipedie"), br(),br(), br(),br(), 
+                           "Created by Martin Sliva" 
+                           
+                           )
+                  
+                  
+      )
     )
   )
 ))
